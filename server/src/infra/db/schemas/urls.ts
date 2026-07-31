6c005402c764dd7ea518b5899dfd0cfee4991ef0
Vinicius Marangoni Tagliéri <vinicius.taglieri@gmail.com>
2026-07-31 00:23:27 -0300

    Initial commit

---
+++ server/src/infra/db/schemas/urls.ts
@@ -0,0 +1,16 @@
+import {
+  integer,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const urls = pgTable("urls", {
+  id: serial("id").primaryKey(),
+  originalUrl: text("original_url").notNull(),
+  shortUrl: varchar("short_url", { length: 100 }).unique().notNull(),
+  accessCount: integer("access_count").default(0).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
---
+++ server/src/infra/http/server.ts
@@ -0,0 +1,46 @@
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { fastify } from "fastify";
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+
+const server = fastify();
+
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: "Validation error",
+      issues: error.validation,
+    });
+  }
+
+  console.error(error);
+
+  return reply.status(500).send({ message: "Internal server error." });
+});
+
+server.register(fastifyCors, { origin: "*" });
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Upload Server",
+      version: "1.0.0",
+    },
+  },
+});
+
+server.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+server.listen({ port: 3333, host: "0.0.0.0" }).then((address) => {
+  console.log(`HTTP Server running! ${address}`);
+});
